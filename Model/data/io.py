9081356be177c406caafc3b30c23c383772bf25c
Charlotte <104776044+shiyiqing111@users.noreply.github.com>
2025-11-23 13:45:18 +0800

    Update io.py

--- Model/data/io.py
+++ Model/data/io.py
@@ -11,5 +11,5 @@ def load_labeled_data(labeled_excel: Path, image_dir: Path):
             continue
         coords = row[["x1","y1","x2","y2","x3","y3","x4","y4","x5","y5","x6","y6"]].values.astype(float)
         records.append({"Filename": fname, "image": img_path, "coords": coords})
-    print(f"✅ Loaded {len(records)} labeled samples.")
+    print(f"Loaded {len(records)} labeled samples.")
     return records
